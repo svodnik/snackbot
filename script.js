f29d04451d9fca6c5cec83c7edfb153c06c98d9a
svodnik <sashavodnik@gmail.com>
2017-09-21 15:30:03 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var myDAVServer = jsDAVlib.getConnection({
+    url: 'webcal://p53-calendars.icloud.com/published/2/hu3E_dBzPi6QTRUEdjwaGVAc0X3Vvg-6uVUcUt9Mu07VJ3zuo5ZSy1-yuB640BpuQaSMMwvxfZw5zyvS42ytRcxG3lLgxXlMByMste1wPsQ',
+    user: '',
+    password: ''
+  });
+  
+  myDAVServer.onready = function() {
+    // Yeah! a correct DAV connection is DONE
+  
+    myDAVServer.getResource(null, function(resource, error) {
+      console.log('Root Resource: ' + JSON.stringify(resource.get()));
+  
+      var data = resource.get();
+  
+      // Recover first child element
+      if (data.meta.type.type === 'dir') {
+        myDAVServer.getResource(data.data[0].href, function(res, error) {
+          // Resource recovered . . .
+          if (data.meta.type.type === 'file' && data.meta.mime === 'text/x-vcard; charset=utf-8') {
+            // data.data contents is a VCARD file !
+          }
+        });
+      }
+    });
+  }
