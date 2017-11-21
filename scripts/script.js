9ea5f1792eca7ff13c5a0e4f250293ba5fb0eb82
svodnik <sashavodnik@gmail.com>
2017-11-20 22:43:58 -0800

    created .env, .gitignored, and updated comments

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,6 @@ var ical = require('ical');
 var moment = require('moment-timezone'); 
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-// caldav url to access
-//var url = 'http://p53-calendars.icloud.com/published/2/2ex0lsKSpKz_G7fuIIgWRjMw9qBcWTRwvcAITf_nt4mWYp5yVBwlrrwbD2l33Op_404hELgNniz2QpyIN4S5b6d-DmBH8MYkE6fCwdMJJw8';
 
 module.exports = function(robot) {
   // create a new snacks array to store selected calendar events
@@ -40,6 +38,8 @@ module.exports = function(robot) {
     );
   }
   // use ical npm module to get calendar data from class calendar
+  // process.env.CALENDAR references CALENDAR config var on Heroku
+  // and in local .env file (which is .gitignored)
   ical.fromURL(process.env.CALENDAR, {}, function(err, data) {
     // handle error
     if (err) {
