75b341f5374ceca2d3fdfae3441f34ab5a7387de
svodnik <sashavodnik@gmail.com>
2017-11-20 20:34:31 -0800

    changed format to address time zone issue (test)

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,8 @@ module.exports = function(robot) {
   function returnEvent(res, k) {
     res.send(
       // return only the first 10 characters, which make up the date
-      '*' + snacks[k].start.toString().substr(0,10) + '*' +
+//      '*' + snacks[k].start.toString().substr(0,10) + '*' +
+      '*' + snacks[k].start + '*' +
       ": " + 
       // return characters 8 to the end of the string, excluding the
       // unneeded text "snacks: " at the start of each entry
@@ -49,6 +50,11 @@ module.exports = function(robot) {
     snacks.sort(function(a,b) {
       return a.start - b.start;
     });
+    console.log(snacks);
+    // for (var el in snacks) {
+    //   snacks[el].start = snacks[el].start.toISOString().substr(0,23) + '-08:00';
+    // }
+    // console.log(snacks);
   });
 
   // respond to the word "cal" in the current channel or DM
